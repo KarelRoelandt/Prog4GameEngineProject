4f5c273fc8be66310820cf1da714456f89379119
KarelRoelandt <karel@trivial.be>
2025-02-16 20:00:22 +0100

    included visual leak detector

--- Minigin/Texture2D.cpp
+++ Minigin/Texture2D.cpp
@@ -1,4 +1,12 @@
 #include <SDL.h>
+
+#if _DEBUG
+// ReSharper disable once CppUnusedIncludeDirective
+#if __has_include(<vld.h>)
+#include <vld.h>
+#endif
+#endif
+
 #include <SDL_image.h>
 #include "Texture2D.h"
 #include "Renderer.h"
